1e6a2a2208500641d68aadf9a7b77dc46b385af4
Isaac <95992083+isaacebi@users.noreply.github.com>
2024-01-06 18:29:06 +0800

    Update test_toke.py

--- src/test_toke.py
+++ src/test_toke.py
@@ -5,7 +5,7 @@ import requests
 TOKEN = os.environ['MET_TOKEN']
 
 URL = "https://api.met.gov.my/v2/data"
-header = {"Authorization": TOKEN}
+headers = {"Authorization": TOKEN}
 params = {
     "datasetid": "FORECAST",
     "datacategoryid": "GENERAL",
@@ -14,10 +14,6 @@ params = {
     "end_date": "2024-01-01",
 }
 
-response = requests.get(
-    URL,
-    header=header,
-    params=params
-)
+response = requests.get(URL, headers=headers, params=params)
 
 print(response.status_code)
