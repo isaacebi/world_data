3f6bcd69424772f45f81341cf322bdc965962ae5
isaacebi <isaac_ebi@rocketmail.com>
2024-01-06 18:17:11 +0800

    Create test_toke.py

---
+++ src/test_toke.py
@@ -0,0 +1,23 @@
+import os
+import pathlib
+import requests
+
+TOKEN = os.environ['MET_TOKEN']
+
+URL = "https://api.met.gov.my/v2/data"
+header = {"Authorization": TOKEN}
+params = {
+    "datasetid": "FORECAST",
+    "datacategoryid": "GENERAL",
+    "locationid": "LOCATION:237",
+    "start_date": "2024-01-01",
+    "end_date": "2024-01-01",
+}
+
+response = requests.get(
+    URL,
+    header,
+    params
+)
+
+print(response.status_code)
