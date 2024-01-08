959b7a2d61bc4805f1eeae48c83eba7507ecf103
isaacebi <isaac_ebi@rocketmail.com>
2024-01-08 14:12:34 +0800

    Update handshake.py

--- src/MET/handshake.py
+++ src/MET/handshake.py
@@ -1,42 +1,31 @@
+# %% Libraries
 import os
 import pathlib
 import requests
 import sqlite3
 import pandas as pd
 
+# internal module
+import getDataMET
 
-print(requests.__version__)
+# check request version
+requestVer = requests.__version__
+text = f'The libraries are {requestVer}'
+cmd = "echo {}".format(text)
+os.system(cmd)
 
-TOKEN = os.environ['MET_TOKEN']
-
-print(len(TOKEN))
-
-headers = {"Authorization": TOKEN}
-params = {
-    "datasetid": "FORECAST",
-    "datacategoryid": "GENERAL",
-    "locationid": "LOCATION:237",
-    "start_date": "2024-01-01",
-    "end_date": "2024-01-01",
-}
-
-URL = f'https://api.met.gov.my/v2.1/data?datasetid={params["datasetid"]}&datacategoryid={params["datacategoryid"]}&locationid={params["locationid"]}&start_date={params["start_date"]}&end_date={params["end_date"]}'
-
-response = requests.get(url=URL, headers=headers)
-
-print(response.status_code)
-
-
-# pathing
+# %% 
+# PATHING
 CURR_FILE = pathlib.Path(__file__).resolve()
-PROJECT_DIR = CURR_FILE.parents[1]
+PROJECT_DIR = CURR_FILE.parents[2]
 DATA_DIR = os.path.join(PROJECT_DIR, 'data')
 DATA_RAW = os.path.join(DATA_DIR, 'raw', 'MET')
+TOKEN_PATH = os.path.join(DATA_DIR, 'MET', 'token.txt')
 
 # DB
 GENERAL_DATA = os.path.join(DATA_RAW, 'general.db')
 
-#
+# %% Helper Function
 def getDB(DB_Path):
     # create connection
     conn = sqlite3.connect(DB_Path)
@@ -46,9 +35,37 @@ def getDB(DB_Path):
 
     return df
 
-print(getDB(GENERAL_DATA))
+# to get token based on state run which either local or gitaction
+def getToken(token=TOKEN_PATH):
+    if 'REQUEST_METHOD' in os.environ :
+        TOKEN = os.environ['MET_TOKEN']
+        print("Proceed with secret token")
+        return TOKEN
+    
+    else :
+        TOKEN = getDataMET.read_text_file(token)
+        print("Proceed with local token")
+        return TOKEN
+        
 
-if 'REQUEST_METHOD' in os.environ :
-    print("This is a webpage")
-else :
-    print("This is not a webpage")
+# %%
+if __name__ == "__main__":
+
+    TOKEN = getToken(token=TOKEN_PATH)
+
+    headers = {"Authorization": TOKEN}
+    params = {
+        "datasetid": "FORECAST",
+        "datacategoryid": "GENERAL",
+        "locationid": "LOCATION:237",
+        "start_date": "2024-01-01",
+        "end_date": "2024-01-01",
+    }
+
+    URL = f'https://api.met.gov.my/v2.1/data?datasetid={params["datasetid"]}&datacategoryid={params["datacategoryid"]}&locationid={params["locationid"]}&start_date={params["start_date"]}&end_date={params["end_date"]}'
+
+    response = requests.get(url=URL, headers=headers)
+
+    text = f'The response code are {response.status_code}'
+    cmd = "echo {}".format(text)
+    os.system(cmd)
