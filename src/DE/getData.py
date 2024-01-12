fbd9e8b9c48792dcf4a5ef6e841f413b8d399ede
isaacebi <isaac_ebi@rocketmail.com>
2024-01-12 23:41:43 +0800

    Update getData.py

--- src/DE/getData.py
+++ src/DE/getData.py
@@ -1,7 +1,6 @@
 # %%
 import os
-import json
-import pathlib
+import re
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -10,20 +9,42 @@ from bs4 import BeautifulSoup
 baseURL = "https://www.dailyexpress.com.my"
 
 # %%
+def displayText(texts):
+    cmd = 'echo {}'.format(texts)
+    os.system(cmd)
+
 # get the "LOCAL SABAH NEWS" section title for each page
+# TODO: implement to get the missing request
 def getTitle(location:str, page:str, url="https://www.dailyexpress.com.my") -> list:
     # creating extracting list
     local_url = url + '/local'
     location_url = local_url + f"/?location={location}"
     page_url = location_url + f'&pageNo={page}'
 
-    # request web
-    r = requests.get(page_url)
-    # to beautiful soup
-    s = BeautifulSoup(r.text, 'html.parser')
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+        "Accept-Encoding": "*",
+        "Connection": "keep-alive"
+    }
+
+    try:
+        # request web
+        r = requests.get(page_url, headers=headers, timeout=5)
+        # to beautiful soup
+        s = BeautifulSoup(r.text, 'html.parser')
+    except:
+        fail_to_scrap = {
+            'location': location,
+            'page': page
+        }
+        return fail_to_scrap
 
     # selecting "LOCAL SABAH NEWS" section
     contents = s.find_all('div', class_='content')
+
+    # catch none errors
+    if len(contents) == 0:
+        return None
 
     newslist = []
 
@@ -47,11 +68,29 @@ def getTitle(location:str, page:str, url="https://www.dailyexpress.com.my") -> l
         
     return newslist
 
+def getLocations(URL="https://www.dailyexpress.com.my/index.cfm") -> list:
+    locations = []
+    
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    # Find all <a> tags with href attribute containing the keyword 'local/?location'
+    targets = soup.find_all('a', href=lambda href: href and 'local/?location' in href)
+
+    # append all location to list
+    for location in targets:
+        textLocation = location.text.strip()
+        textLocation = re.sub(r'\s', '%20', textLocation)
+        locations.append(textLocation)
+
+    return locations
+
 # %%
-titles = getTitle(
-    location = "Kota%20Kinabalu",
-    page = "1",
-    url = baseURL
-)
+
+if __name__ == "__main__":
+    titles = getTitle(
+        location = "Kota%20Kinabalu",
+        page = "1",
+        url = baseURL
+    )
 
 # %%
